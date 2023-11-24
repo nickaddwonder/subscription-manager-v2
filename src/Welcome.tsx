b08fc3cbc7db3a24dd1e39bbfe4d5da21e5dab77
Nick Zou <z.nick00@gmail.com>
2023-11-24 10:45:40 -0500

    add eslint-config-prettier

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,6 +1,17 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { database } from "./firebase/firebase";
 
 const Welcome: FC = () => {
+  useEffect(() => {
+    const whatever = async () => {
+      const docRef = doc(database, "content_lists", "6uml7boTWJgcR7lnP3la");
+      const docSnap = await getDoc(docRef);
+      console.log(docSnap.data());
+    };
+
+    whatever();
+  }, []);
   return <div>Welcome to logged in!</div>;
 };
 
