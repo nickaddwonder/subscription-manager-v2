99ab47deb12ac1faba144af8136c8bc7006e9aed
Nick Zou <z.nick00@gmail.com>
2023-12-02 14:03:37 -0500

    add usehooks-ts and navigation functionality

--- src/components/layouts/Header/Menu/Menu.tsx
+++ src/components/layouts/Header/Menu/Menu.tsx
@@ -1,27 +1,39 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import HamburgerMenu from "@icons/HamburgerMenu/HamburgerMenu";
 import MenuItem from "./MenuItem/MenuItem";
 import { Link } from "react-router-dom";
 
 const Menu: FC = () => {
+  const [open, setOpen] = useState(false);
+  const handleMobileMenuToggle = () => {
+    setOpen((open) => !open);
+  };
   return (
     <div className="relative my-auto flex items-center justify-center">
-      <button onClick={() => console.log("me clicked!")}>
+      <button onClick={handleMobileMenuToggle}>
         <HamburgerMenu />
       </button>
-      <nav className="absolute right-0 top-full rounded bg-white shadow-[4px_4px_3px_0px_rgba(0,0,0,0.10)]">
-        <ul className="flex flex-col gap-3 p-3">
-          <MenuItem>
-            <Link to="/dashboard">Dashboard</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/watchlist">Watch List</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/reports">Reports</Link>
-          </MenuItem>
-        </ul>
-      </nav>
+      {open && (
+        <nav className="absolute right-0 top-full rounded bg-white shadow-[4px_4px_3px_0px_rgba(0,0,0,0.10)]">
+          <ul className="flex flex-col gap-3 p-3">
+            <MenuItem>
+              <Link to="/dashboard" onClick={() => setOpen(false)}>
+                Dashboard
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/watchlist" onClick={() => setOpen(false)}>
+                Watch List
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/reports" onClick={() => setOpen(false)}>
+                Reports
+              </Link>
+            </MenuItem>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
