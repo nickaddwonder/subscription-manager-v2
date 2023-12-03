cb07fc76a0bbd54322df6796513b79392934e837
Nick Zou <z.nick00@gmail.com>
2023-12-03 14:02:25 -0500

    add tmdb types

---
+++ src/types/tmdb/TvShow.ts
@@ -0,0 +1,18 @@
+type TvShow = {
+  id: number;
+  name: string;
+  original_name: string;
+  poster_path: string;
+  popularity: number;
+  backdrop_path: string;
+  vote_average: number;
+  overview: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_language: string;
+  vote_count: number;
+  first_air_date: string;
+  adult: boolean;
+};
+
+export default TvShow;
